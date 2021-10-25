ee78a341bc67141d3705e71ffbe4d81127ac7804
TerentevVlad <ter.vlad.ter@gmail.com>
2021-10-25 16:10:56 +0300

    Изменил интерфейс IDataStorageService

--- Runtime/IDataStorageService.cs
+++ Runtime/IDataStorageService.cs
@@ -6,7 +6,7 @@ namespace LittleBit.Modules.CoreModule
     {
         public T GetData<T>(string key) where T : Data, new();
         public void SetData<T>(string key, T data) where T : Data;
-        public void AddUpdateDataListener(string key, Action onUpdateData);
-        public void RemoveUpdateDataListener(string key, Action onUpdateData);
+        public void AddUpdateDataListener(string key, Action <string> onUpdateData);
+        public void RemoveUpdateDataListener(string key, Action <string> onUpdateData);
     }
 }
