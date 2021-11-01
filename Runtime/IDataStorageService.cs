e62d7e733b118616218e83d82836c184426677e0
ariatophanes <ariatophanes@gmail.com>
2021-11-01 14:39:09 +0300

    Added GenericCallback and AddUpdateDataListener<T> in IDataStorageService.cs

--- Runtime/IDataStorageService.cs
+++ Runtime/IDataStorageService.cs
@@ -4,9 +4,11 @@ namespace LittleBit.Modules.CoreModule
 {
     public interface IDataStorageService : IService
     {
+        public delegate void GenericCallback<T>(T item);
         public T GetData<T>(string key) where T : Data, new();
         public void SetData<T>(string key, T data) where T : Data;
         public void AddUpdateDataListener(string key, Action <string> onUpdateData);
+        public void AddUpdateDataListener<T>(string key, GenericCallback<T> onUpdateData);
         public void RemoveUpdateDataListener(string key, Action <string> onUpdateData);
     }
 }
