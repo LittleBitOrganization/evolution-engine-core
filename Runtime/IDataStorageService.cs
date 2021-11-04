e5e31643c0146f862a771e225937edc0198a0524
TerentevVlad <ter.vlad.ter@gmail.com>
2021-11-04 21:09:33 +0300

    Убрана подписка с возвращением ключа

--- Runtime/IDataStorageService.cs
+++ Runtime/IDataStorageService.cs
@@ -1,5 +1,3 @@
-using System;
-
 namespace LittleBit.Modules.CoreModule
 {
     public interface IDataStorageService : IService
@@ -7,8 +5,8 @@ namespace LittleBit.Modules.CoreModule
         public delegate void GenericCallback<T>(T item);
         public T GetData<T>(string key) where T : Data, new();
         public void SetData<T>(string key, T data) where T : Data;
-        public void AddUpdateDataListener(string key, Action <string> onUpdateData);
+        //public void AddUpdateDataListener(string key, Action <string> onUpdateData);
         public void AddUpdateDataListener<T>(string key, GenericCallback<T> onUpdateData);
-        public void RemoveUpdateDataListener(string key, Action <string> onUpdateData);
+        public void RemoveUpdateDataListener<T>(string key, GenericCallback<T> onUpdateData);
     }
 }
