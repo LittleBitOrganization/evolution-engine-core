7b5da18ff255d68a8cd579c09f389b2f9a9e4111
TerentevVlad <ter.vlad.ter@gmail.com>
2021-11-16 18:59:46 +0300

    Добавил ISaveService

---
+++ Runtime/ISaveService.cs
@@ -0,0 +1,8 @@
+namespace LittleBit.Modules.CoreModule
+{
+    public interface ISaveService : IService
+    {
+        public void SaveData(string key, object data);
+        public T LoadData<T>(string key) where T : Data;
+    }
+}
