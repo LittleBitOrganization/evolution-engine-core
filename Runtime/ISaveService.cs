4ae3d5f9df78927cedeb6917e4a7c5d42480f8d7
TerentevVlad <ter.vlad.ter@gmail.com>
2021-11-16 20:00:11 +0300

    Удалён AddSavableObject из ISaveService.cs

--- Runtime/ISaveService.cs
+++ Runtime/ISaveService.cs
@@ -2,7 +2,6 @@ namespace LittleBit.Modules.CoreModule
 {
     public interface ISaveService : IService
     {
-        public void AddSavableObject(ISavable savableObject);
         public void SaveData(string key, object data);
         public T LoadData<T>(string key) where T : Data;
     }
