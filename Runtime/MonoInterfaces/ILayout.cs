1e12b31ffe26c4c52951892cdf679a0de6c6c0e7
TerentevVlad <ter.vlad.ter@gmail.com>
2022-03-10 14:37:51 +0300

    Добавил ILayout

---
+++ Runtime/MonoInterfaces/ILayout.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace LittleBit.Modules.CoreModule.MonoInterfaces
+{
+    public interface ILayout : ITransformable, IDestroyable, IActivatable
+    {
+        public void AppendChildElement(ILayout layout);
+        public void RemoveElement(ILayout layout);
+        public ILayout GetChildElementLayout(int index);
+        public List<ILayout> GetAllChildrenElements();
+        public void ShowChildElements();
+        public void HideChildElements();
+        public void SetParent(ILayout layout);
+    }
+}
