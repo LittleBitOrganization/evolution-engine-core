0bb5d130c18abc51c7d81728ee6ec89149708224
TerentevVlad <ter.vlad.ter@gmail.com>
2022-03-16 10:41:16 +0300

    fix method  AddUpdateDataListenerWithUpdateData in IDataStorageService

--- Runtime/IDataStorageService.cs
+++ Runtime/IDataStorageService.cs
@@ -9,7 +9,7 @@ namespace LittleBit.Modules.CoreModule
         public void RemoveData<T>(string key) where T : Data;
         public void SetData<T>(string key, T data, SaveMode saveMode = SaveMode.Save) where T : Data;
         public void AddUpdateDataListener<T>(object handler, string key, GenericCallback<T> onUpdateData);
-        public void AddUpdateDataListenerWithUpdateData<T>(object handler, string key, GenericCallback<T> onUpdateData);
+        public void AddUpdateDataListenerWithUpdateData<T>(object handler, string key, GenericCallback<T> onUpdateData) where T : Data, new();
         public void RemoveUpdateDataListener<T>(object handler, string key, GenericCallback<T> onUpdateData);
         public void RemoveAllUpdateDataListenersOnObject(object handler);
         StorageData<T> CreateDataWrapper<T>(object handler, string key) where T : Data, new();
