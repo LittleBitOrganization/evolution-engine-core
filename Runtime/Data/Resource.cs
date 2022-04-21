c8de673a0e67a1710be2d94c3203a8642de1e4d4
TerentevVlad <ter.vlad.ter@gmail.com>
2022-04-21 17:52:22 +0300

    Добавил скрипт ресурсов в зависимости 1.2.0

---
+++ Runtime/Data/Resource.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LittleBit.Modules.CoreModule
+{
+    [Serializable]
+    public class Resource : Data
+    {
+        public Resource(string id)
+        {
+            Id = id;
+        }
+
+        public double Value;
+
+        public string Id;
+
+        public Resource()
+        {
+            
+        }
+    }
+}
