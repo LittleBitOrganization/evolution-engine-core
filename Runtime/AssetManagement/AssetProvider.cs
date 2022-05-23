f89a5ef1a8d9d10bae432e01766de5d48915e149
plussamara <alligator85@list.ru>
2022-05-23 20:17:13 +0300

    Update AssetProvider.cs

--- Runtime/AssetManagement/AssetProvider.cs
+++ Runtime/AssetManagement/AssetProvider.cs
@@ -4,6 +4,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Scripting;
 
 namespace LittleBit.Modules.CoreModule.AssetManagement
 {
