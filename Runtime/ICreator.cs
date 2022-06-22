91c9ebc5e1b40e3c15e4099c43cca8aa628ca33f
TerentevVlad <ter.vlad.ter@gmail.com>
2022-06-22 17:37:24 +0300

    Расширил ICreator

--- Runtime/ICreator.cs
+++ Runtime/ICreator.cs
@@ -3,5 +3,10 @@ using UnityEngine;
 public interface ICreator
 {
     public GameObject InstantiatePrefab(Object prefab);
+    public GameObject InstantiatePrefab(Object prefab, Transform parentTransform);
+
+    public GameObject InstantiatePrefab(Object prefab, Vector3 position, Quaternion rotation,
+        Transform parentTransform);
+    
     public T Instantiate<T>(params object[] args);
 }
