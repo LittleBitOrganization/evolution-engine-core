79b55e44e4fe3250b77c12d071dc98efb86cb0bf
ariatophanes <ariatophanes@gmail.com>
2022-08-29 17:15:59 +0300

    Added events to IReadOnlyContainer

--- Runtime/IReadOnlyContainer.cs
+++ Runtime/IReadOnlyContainer.cs
@@ -1,12 +1,17 @@
+using System;
 using System.Collections.ObjectModel;
 
 namespace LittleBit.Modules.CoreModule
 {
     public interface IReadOnlyContainer<T1, T2>
     {
+        public event Action<T1, T2> OnItemAdded;
+        
         ReadOnlyDictionary<T1, T2> Items { get; }
 
         T2 TryGetItem(T1 key);
+
+        void GetItemWhenAdded(T1 key, Action<T2> onAdded);
 
         void TryRemoveItem(T1 key);
     }
