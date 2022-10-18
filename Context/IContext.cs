2c090516c7c6de7d96d688ea2856a5886f4e9021
TerentevVlad <ter.vlad.ter@gmail.com>
2022-10-18 14:48:56 +0300

    Изменил интерфейс IContext

--- Context/IContext.cs
+++ Context/IContext.cs
@@ -2,7 +2,8 @@ namespace LittleBit.Context
 {
     public interface IContext
     {
-        public void Bind<T>(T service);
+        public void BindFromInstance<T>(T service);
+        public void Bind<T>();
         public void Unbind<T>();
         public T Resolve<T>();
         public IContext CreateContext();
